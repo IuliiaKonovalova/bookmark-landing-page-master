081cf1d433dfc5c04378ffdcd6b5ef27025b48f3
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-14 21:18:03 +0800

    Add event listeners for Esc btn and logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const navbar = document.getElementById("navbar");
 const dropdownMenu = document.getElementById("hamburger");
 const closeMenuBtn = document.getElementById("menu-close");
+const closeMenuLogo = document.getElementById("logo--close");
 
 dropdownMenu.addEventListener("click", function (e) {
   showMenu(e);
@@ -10,6 +11,15 @@ closeMenuBtn.addEventListener("click", function (e) {
   closeMenu(e);
 });
 
+closeMenuLogo.addEventListener("click", function (e) {
+  closeMenu(e);
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    closeMenu(e);
+  }
+})
 const showMenu = function (e) {
   e.preventDefault();
   navbar.classList.add("mobile");
