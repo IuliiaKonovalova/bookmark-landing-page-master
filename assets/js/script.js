05ce71db0351307e25baaa3b150e2ef5938d7b83
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-15 16:18:15 +0800

    Add event listener on load

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,23 +8,32 @@ const tabsContainer = document.querySelector('.features__tab-container');
 const tabs = document.querySelectorAll('.features__tab');
 const tabsContent = document.querySelectorAll('.feature__content');
 
-dropdownMenu.addEventListener("click", function (e) {
-  showMenu(e);
-});
 
-closeMenuBtn.addEventListener("click", function (e) {
-  closeMenu(e);
-});
+document.addEventListener('DOMContentLoaded', function () {
 
-closeMenuLogo.addEventListener("click", function (e) {
-  closeMenu(e);
-});
+  dropdownMenu.addEventListener("click", function (e) {
+    showMenu(e);
+  });
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape') {
+  closeMenuBtn.addEventListener("click", function (e) {
     closeMenu(e);
-  }
+  });
+
+  closeMenuLogo.addEventListener("click", function (e) {
+    closeMenu(e);
+  });
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+      closeMenu(e);
+    }
+  })
+
+  tabsContainer.addEventListener('click', function (e) {
+    changeTabContent(e);
+  });
 })
+
 const showMenu = function (e) {
   e.preventDefault();
   navbar.classList.add("mobile");
@@ -35,27 +44,18 @@ const closeMenu = function (e) {
   navbar.classList.remove("mobile");
 }
 
-tabsContainer.addEventListener('click', function (e) {
-  changeTabContent(e);
-
-});
-
 const changeTabContent = function (e) {
   const clicked = e.target.closest('.features__tab');
-  console.log(clicked);
   if (!clicked) return;
 
   // Remove Active Class
-
   tabs.forEach((t) => t.classList.remove('features__tab--active'));
   tabsContent.forEach((c) => c.classList.remove('feature__content--active'));
 
   // Activate Tab
-
   clicked.classList.add('features__tab--active');
 
   // Activate Content
-
   document
     .querySelector(`.feature__content--${clicked.dataset.tab}`)
     .classList.add('feature__content--active');
